6fefdd58bbb28ec3d014a8c998bce430a1f966ad
Artawood Chitamitara <artawood@Artawoods-MacBook-Pro.local>
2018-12-04 23:12:52 -0800

    test 12

--- server/index.js
+++ server/index.js
@@ -71,9 +71,12 @@ class App {
 
     if (process.env.NODE_ENV === "production") {
       app.use(express.static("client/build")); // serve the static react app
-      app.get(/^\/(?!api).*/, (req, res) => {
-        // don't serve api routes to react app
-        res.sendFile(path.join(__dirname, "./client/build/index.html"));
+      // app.get(/^\/(?!api).*/, (req, res) => {
+      //   // don't serve api routes to react app
+      //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+      // });
+      app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
       });
       console.log("Serving React App...");
     }
